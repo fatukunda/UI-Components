0a9ce56d5acba5d552dc46ddd52ffe66df816b1c
Frank Atukunda <fatukunda@gmail.com>
2020-07-26 15:16:06 +0300

    Add tabs

---
+++ src/components/Tabs/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="navigator">
+    <div class="nav-item" @click="selectedItem = 'shipping'">
+      <h4 :class="{ 'selected-text': selectedItem === 'shipping' }">
+        Shipping
+      </h4>
+      <div
+        class="not-selected"
+        :class="{ selected: selectedItem === 'shipping' }"
+      ></div>
+    </div>
+    <div class="nav-item" @click="selectedItem = 'payment'">
+      <h4 :class="{ 'selected-text': selectedItem === 'payment' }">Payment</h4>
+      <div
+        class="not-selected"
+        :class="{ selected: selectedItem === 'payment' }"
+      ></div>
+    </div>
+    <div class="nav-item" @click="selectedItem = 'confirmation'">
+      <h4 :class="{ 'selected-text': selectedItem === 'confirmation' }">
+        Confirmation
+      </h4>
+      <div
+        class="not-selected"
+        :class="{ selected: selectedItem === 'confirmation' }"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTabs",
+  data() {
+    return {
+      selectedItem: "shipping"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.navigator {
+  display: flex;
+}
+.nav-item {
+  margin: 0.5rem;
+  min-width: 110px;
+  cursor: pointer;
+}
+h6 {
+  text-align: center;
+}
+.not-selected {
+  border-bottom: 2px solid #efefef;
+  margin-top: 0.5rem;
+}
+.selected {
+  border-bottom: 2px solid #38a7c8;
+  margin-top: 0.5rem;
+}
+.selected-text {
+  color: #2f3771;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Tabs />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Tabs from "@/components/Tabs";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Tabs
   }
 };
 </script>
